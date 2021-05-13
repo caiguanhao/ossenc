35ee756507590598e336b359b6a174aeab9fc821
Cai Guanhao (Choi Goon-ho) <caiguanhao@gmail.com>
2021-05-13 19:04:36 +0800

    use github.com/gopsql/goconf

--- config.go
+++ config.go
@@ -3,16 +3,11 @@ package main
 import (
 	"crypto/rand"
 	"encoding/hex"
-	"fmt"
-	"go/ast"
-	"go/parser"
-	"go/token"
 	"io/ioutil"
 	"log"
 	"os"
-	"reflect"
-	"strconv"
-	"strings"
+
+	"github.com/gopsql/goconf"
 )
 
 type (
@@ -48,77 +43,6 @@ func (k key) String() string {
 	return hex.EncodeToString(k)
 }
 
-func unmarshalConfig(input []byte, v interface{}) error {
-	fset := token.NewFileSet()
-	f, err := parser.ParseFile(fset, "", string(input), 0)
-	if err != nil {
-		return err
-	}
-	rv := reflect.ValueOf(v)
-	for _, decl := range f.Decls {
-		gd, ok := decl.(*ast.GenDecl)
-		if !ok {
-			continue
-		}
-		if gd.Tok != token.CONST {
-			continue
-		}
-		for _, sp := range gd.Specs {
-			vs, ok := sp.(*ast.ValueSpec)
-			if !ok {
-				continue
-			}
-			for _, v := range vs.Values {
-				bl, ok := v.(*ast.BasicLit)
-				if !ok {
-					continue
-				}
-				value, err := strconv.Unquote(bl.Value)
-				if err != nil {
-					return err
-				}
-				for _, name := range vs.Names {
-					field := rv.Elem().FieldByName(name.Name)
-					if field.Kind() == reflect.String {
-						field.SetString(value)
-					} else if i, ok := field.Addr().Interface().(interface{ SetString(string) error }); ok {
-						err := i.SetString(value)
-						if err != nil {
-							return err
-						}
-					}
-				}
-			}
-		}
-	}
-	return nil
-}
-
-func marshalConfig(c config) (output string) {
-	output = `package config
-
-const (
-`
-	rt := reflect.TypeOf(c)
-	rv := reflect.ValueOf(c)
-	for i := 0; i < rt.NumField(); i++ {
-		if i > 0 {
-			output += "\n"
-		}
-		if tag := string(rt.Field(i).Tag); tag != "" {
-			lines := strings.Split(strings.TrimSpace(tag), "\n")
-			for _, line := range lines {
-				output += "\t// " + line + "\n"
-			}
-		}
-		v := fmt.Sprint(rv.Field(i).Interface())
-		output += "\t" + rt.Field(i).Name + " = " + strconv.Quote(v) + "\n"
-	}
-	output += `)
-`
-	return
-}
-
 func readConf(file string, toUpdate bool) (conf config) {
 	created := false
 	content, err := ioutil.ReadFile(file)
@@ -132,7 +56,7 @@ func readConf(file string, toUpdate bool) (conf config) {
 			log.Fatalln(err)
 		}
 	} else {
-		err = unmarshalConfig([]byte(content), &conf)
+		err = goconf.Unmarshal([]byte(content), &conf)
 		if err != nil {
 			log.Fatalln(err)
 		}
@@ -143,7 +67,11 @@ func readConf(file string, toUpdate bool) (conf config) {
 			rand.Read(k)
 			conf.EncryptionKey = k
 		}
-		err = ioutil.WriteFile(file, []byte(marshalConfig(conf)), 0600)
+		content, err := goconf.Marshal(conf)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		err = ioutil.WriteFile(file, content, 0600)
 		if err != nil {
 			log.Fatalln(err)
 		}
--- go.mod
+++ go.mod
@@ -5,5 +5,6 @@ go 1.15
 require (
 	github.com/caiguanhao/ossslim v1.0.1
 	github.com/caiguanhao/strftime v1.1.0
+	github.com/gopsql/goconf v1.0.0
 	golang.org/x/term v0.0.0-20210422114643-f5beecf764ed
 )
